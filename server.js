ac48750d494c037701b29184d282297bd480b6c4
PRATANGGA <pratanggajulang31@gmail.com>
2025-06-25 10:21:38 +0700

    fix server.js

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
+const merge = require('./merge-json.cjs');
 const jsonServer = require('json-server');
 const path = require('path');
-const fs = require('fs');
 
-const merge = require('./merge-json.cjs'); // panggil merge sebelum server start
-merge(); // hasilkan db.json
+merge(); // update db.json tiap run
 
 const server = jsonServer.create();
+server.use(jsonServer.defaults());
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
 server.use(router);
+
 const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running at http://localhost:${PORT}`);
-});
+server.listen(PORT, () => console.log(`JSON Server di port ${PORT}`));
 
